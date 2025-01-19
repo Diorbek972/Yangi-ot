ba492ddee2c8483b06a72bb7ec4fddad6b77a16c
diorbek <diorbeknorhuja@gmail.com>
2025-01-19 14:26:31 +0500

    tortinchi qismini tugatm

--- src/components/Clubs.jsx
+++ src/components/Clubs.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function Clubs({img, deploy, texts}) {
   return (
-    <div>
-      <img src={img} alt="" />
-      <h3>{deploy}</h3>
+    <div className='cards p-4 rounded-2'>
+      <img className='w-100' src={img} alt="" />
+      <h3 className='my-3'>{deploy}</h3>
+      <h2 className='chiziqcha text-center pb-3'>__</h2>
       <p>{texts}</p>
     </div>
   )
