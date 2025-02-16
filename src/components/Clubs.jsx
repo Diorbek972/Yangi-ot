c73e35ccfa234e37c20d2e49c5d4bbcb40da477f
diorbek <diorbeknorhuja@gmail.com>
2025-02-16 20:44:41 +0500

    salp

--- src/components/Clubs.jsx
+++ src/components/Clubs.jsx
@@ -5,7 +5,7 @@ export default function Clubs({img, deploy, texts}) {
     <div className='cards p-4 rounded-2'>
       <img className='w-100' src={img} alt="" />
       <h3 className='my-3'>{deploy}</h3>
-      <h2 className='chiziqcha text-center pb-3'>__</h2>
+      <h2 className='chiziqcha text-center pb-3'>___</h2>
       <p>{texts}</p>
     </div>
   )
